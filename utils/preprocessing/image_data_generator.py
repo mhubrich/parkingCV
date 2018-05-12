362a9d582ec3781fc488d7ad65f3158d0a58f5fb
Markus Hubrich <mhubrich@paybyphone.com>
2018-05-11 19:24:30 -0700

    generator for augmentation. TODO: standardize() and transform()

---
+++ utils/preprocessing/image_data_generator.py
@@ -0,0 +1,372 @@
+"""
+Keras's ImageDataGenerator modified to support six channels.
+Source: https://github.com/keras-team/keras/blob/master/keras/preprocessing/image.py
+"""
+import numpy as np
+
+
+class ImageDataGenerator(object):
+    """Generate batches of tensor image data with real-time data augmentation.
+     The data will be looped over (in batches).
+    # Arguments
+        featurewise_center: Boolean.
+            Set input mean to 0 over the dataset, feature-wise.
+        samplewise_center: Boolean. Set each sample mean to 0.
+        featurewise_std_normalization: Boolean.
+            Divide inputs by std of the dataset, feature-wise.
+        samplewise_std_normalization: Boolean. Divide each input by its std.
+        zca_epsilon: epsilon for ZCA whitening. Default is 1e-6.
+        zca_whitening: Boolean. Apply ZCA whitening.
+        rotation_range: Int. Degree range for random rotations.
+        channel_shift_range: Float. Range for random channel shifts.
+        fill_mode: One of {"constant", "nearest", "reflect" or "wrap"}.
+            Default is 'nearest'.
+            Points outside the boundaries of the input are filled
+            according to the given mode:
+            'constant': kkkkkkkk|abcd|kkkkkkkk (cval=k)
+            'nearest':  aaaaaaaa|abcd|dddddddd
+            'reflect':  abcddcba|abcd|dcbaabcd
+            'wrap':  abcdabcd|abcd|abcdabcd
+        cval: Float or Int.
+            Value used for points outside the boundaries
+            when `fill_mode = "constant"`.
+        horizontal_flip: Boolean. Randomly flip inputs horizontally.
+        vertical_flip: Boolean. Randomly flip inputs vertically.
+        rescale: rescaling factor. Defaults to None.
+            If None or 0, no rescaling is applied,
+            otherwise we multiply the data by the value provided
+            (before applying any other transformation).
+        data_format: Image data format,
+            either "channels_first" or "channels_last".
+            "channels_last" mode means that the images should have shape
+            `(samples, height, width, channels)`,
+            "channels_first" mode means that the images should have shape
+            `(samples, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+        validation_split: Float. Fraction of images reserved for validation
+            (strictly between 0 and 1).
+    """
+    def __init__(self,
+                 featurewise_center=False,
+                 samplewise_center=False,
+                 featurewise_std_normalization=False,
+                 samplewise_std_normalization=False,
+                 zca_whitening=False,
+                 zca_epsilon=1e-6,
+                 rotation_range=0.,
+                 brightness_range=None,
+                 channel_shift_range=0.,
+                 fill_mode='nearest',
+                 cval=0.,
+                 horizontal_flip=False,
+                 vertical_flip=False,
+                 rescale=None,
+                 data_format=None,
+                 validation_split=0.0):
+        if data_format is None:
+            data_format = K.image_data_format()
+        self.featurewise_center = featurewise_center
+        self.samplewise_center = samplewise_center
+        self.featurewise_std_normalization = featurewise_std_normalization
+        self.samplewise_std_normalization = samplewise_std_normalization
+        self.zca_whitening = zca_whitening
+        self.zca_epsilon = zca_epsilon
+        self.rotation_range = rotation_range
+        self.brightness_range = brightness_range
+        self.channel_shift_range = channel_shift_range
+        self.fill_mode = fill_mode
+        self.cval = cval
+        self.horizontal_flip = horizontal_flip
+        self.vertical_flip = vertical_flip
+        self.rescale = rescale
+
+        if data_format not in {'channels_last', 'channels_first'}:
+            raise ValueError(
+                '`data_format` should be `"channels_last"` '
+                '(channel after row and column) or '
+                '`"channels_first"` (channel before row and column). '
+                'Received: %s' % data_format)
+        self.data_format = data_format
+        if data_format == 'channels_first':
+            self.channel_axis = 1
+            self.row_axis = 2
+            self.col_axis = 3
+        if data_format == 'channels_last':
+            self.channel_axis = 3
+            self.row_axis = 1
+            self.col_axis = 2
+        if validation_split and not 0 < validation_split < 1:
+            raise ValueError(
+                '`validation_split` must be strictly between 0 and 1. '
+                ' Received: %s' % validation_split)
+        self._validation_split = validation_split
+
+        self.mean = None
+        self.std = None
+        self.principal_components = None
+
+        if zca_whitening:
+            if not featurewise_center:
+                self.featurewise_center = True
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`zca_whitening`, which overrides '
+                              'setting of `featurewise_center`.')
+            if featurewise_std_normalization:
+                self.featurewise_std_normalization = False
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`zca_whitening` '
+                              'which overrides setting of'
+                              '`featurewise_std_normalization`.')
+        if featurewise_std_normalization:
+            if not featurewise_center:
+                self.featurewise_center = True
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`featurewise_std_normalization`, '
+                              'which overrides setting of '
+                              '`featurewise_center`.')
+        if samplewise_std_normalization:
+            if not samplewise_center:
+                self.samplewise_center = True
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`samplewise_std_normalization`, '
+                              'which overrides setting of '
+                              '`samplewise_center`.')
+
+    def flow(self, x, y=None, batch_size=32, shuffle=True, sample_weight=None, seed=None,
+             save_to_dir=None, save_prefix='', save_format='png', subset=None):
+        raise NotImplementedError('Use `flow_from_directory` to generate batches.')
+
+    def flow_from_directory(self, directory,
+                            target_size=(256, 256), class_mode='binary',
+                            tags=(('satellite', 'jpg'), ('roadmap', 'png')),
+                            batch_size=32, shuffle=True, seed=None,
+                            save_to_dir=None,
+                            save_prefix='',
+                            save_format='png',
+                            subset=None,
+                            interpolation='nearest'):
+        """Takes the path to a directory & generates batches of augmented data.
+        # Arguments
+            directory: Path to the target directory.
+                It should contain one subdirectory per class.
+                Any PNG, JPG, BMP, PPM or TIF images
+                inside each of the subdirectories directory tree
+                will be included in the generator.
+                See [this script](https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d)
+                for more details.
+            target_size: Tuple of integers `(height, width)`,
+                default: `(256, 256)`.
+                The dimensions to which all images found will be resized.
+            class_mode: One of "categorical", "binary", "sparse",
+                "input", or None. Default: "categorical".
+                Determines the type of label arrays that are returned:
+                - "binary" will be 1D binary labels,
+                    "sparse" will be 1D integer labels,
+                If None, no labels are returned
+            batch_size: Size of the batches of data (default: 32).
+            shuffle: Whether to shuffle the data (default: True)
+            seed: Optional random seed for shuffling and transformations.
+            save_to_dir: None or str (default: None).
+                This allows you to optionally specify
+                a directory to which to save
+                the augmented pictures being generated
+                (useful for visualizing what you are doing).
+            save_prefix: Str. Prefix to use for filenames of saved pictures
+                (only relevant if `save_to_dir` is set).
+            save_format: One of "png", "jpeg"
+                (only relevant if `save_to_dir` is set). Default: "png".
+            subset: Subset of data (`"training"` or `"validation"`) if
+                `validation_split` is set in `ImageDataGenerator`.
+            interpolation: Interpolation method used to
+                resample the image if the
+                target size is different from that of the loaded image.
+                Supported methods are `"nearest"`, `"bilinear"`,
+                and `"bicubic"`.
+        # Returns
+            A `DirectoryIterator` yielding tuples of `(x, y)`
+                where `x` is a numpy array containing a batch
+                of images with shape `(batch_size, *target_size, channels)`
+                and `y` is a numpy array of corresponding labels.
+        """
+        return DirectoryIterator(
+            directory, self,
+            target_size=target_size, class_mode=class_mode,
+            tags=tags,
+            data_format=self.data_format,
+            batch_size=batch_size, shuffle=shuffle, seed=seed,
+            save_to_dir=save_to_dir,
+            save_prefix=save_prefix,
+            save_format=save_format,
+            subset=subset,
+            interpolation=interpolation)
+
+    def standardize(self, x):
+        """Applies the normalization configuration to a batch of inputs.
+        # Arguments
+            x: Batch of inputs to be normalized.
+        # Returns
+            The inputs, normalized.
+        """
+        if self.rescale:
+            x *= self.rescale
+        if self.samplewise_center:
+            x -= np.mean(x, keepdims=True)
+        if self.samplewise_std_normalization:
+            x /= (np.std(x, keepdims=True) + K.epsilon())
+
+        if self.featurewise_center:
+            if self.mean is not None:
+                x -= self.mean
+            else:
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`featurewise_center`, but it hasn\'t '
+                              'been fit on any training data. Fit it '
+                              'first by calling `.fit(numpy_data)`.')
+        if self.featurewise_std_normalization:
+            if self.std is not None:
+                x /= (self.std + K.epsilon())
+            else:
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`featurewise_std_normalization`, '
+                              'but it hasn\'t '
+                              'been fit on any training data. Fit it '
+                              'first by calling `.fit(numpy_data)`.')
+        if self.zca_whitening:
+            if self.principal_components is not None:
+                flatx = np.reshape(x, (-1, np.prod(x.shape[-3:])))
+                whitex = np.dot(flatx, self.principal_components)
+                x = np.reshape(whitex, x.shape)
+            else:
+                warnings.warn('This ImageDataGenerator specifies '
+                              '`zca_whitening`, but it hasn\'t '
+                              'been fit on any training data. Fit it '
+                              'first by calling `.fit(numpy_data)`.')
+        return x
+
+    def random_transform(self, x, seed=None):
+        """Randomly augments a single image tensor.
+        # Arguments
+            x: 3D tensor, single image.
+            seed: Random seed.
+        # Returns
+            A randomly transformed version of the input (same shape).
+        """
+        # x is a single image, so it doesn't have image number at index 0
+        img_row_axis = self.row_axis - 1
+        img_col_axis = self.col_axis - 1
+        img_channel_axis = self.channel_axis - 1
+
+        if seed is not None:
+            np.random.seed(seed)
+
+        # Use composition of homographies
+        # to generate final transform that needs to be applied
+        if self.rotation_range:
+            theta = np.deg2rad(np.random.uniform(
+                -self.rotation_range,
+                self.rotation_range))
+        else:
+            theta = 0
+
+        transform_matrix = None
+        if theta != 0:
+            rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
+                                        [np.sin(theta), np.cos(theta), 0],
+                                        [0, 0, 1]])
+            transform_matrix = rotation_matrix
+
+        if transform_matrix is not None:
+            h, w = x.shape[img_row_axis], x.shape[img_col_axis]
+            transform_matrix = transform_matrix_offset_center(
+                transform_matrix, h, w)
+            x = apply_transform(x, transform_matrix, img_channel_axis,
+                                fill_mode=self.fill_mode, cval=self.cval)
+
+        if self.channel_shift_range != 0:
+            x = random_channel_shift(x,
+                                     self.channel_shift_range,
+                                     img_channel_axis)
+        if self.horizontal_flip:
+            if np.random.random() < 0.5:
+                x = flip_axis(x, img_col_axis)
+
+        if self.vertical_flip:
+            if np.random.random() < 0.5:
+                x = flip_axis(x, img_row_axis)
+
+        if self.brightness_range is not None:
+            x = random_brightness(x, self.brightness_range)
+
+        return x
+
+    def fit(self, x,
+            augment=False,
+            rounds=1,
+            seed=None):
+        """Computes the internal data stats related to the data-dependent transformations, based on an array of sample data.
+        Only required if `featurewise_center` or
+        `featurewise_std_normalization` or `zca_whitening` are set to True.
+        # Arguments
+            x: Sample data. Should have rank 4.
+             In case of grayscale data,
+             the channels axis should have value 1, and in case
+             of RGB data, it should have value 3.
+            augment: Boolean (default: False).
+                Whether to fit on randomly augmented samples.
+            rounds: Int (default: 1).
+                If using data augmentation (`augment=True`),
+                this is how many augmentation passes over the data to use.
+            seed: Int (default: None). Random seed.
+       """
+        x = np.asarray(x, dtype=K.floatx())
+        if x.ndim != 4:
+            raise ValueError('Input to `.fit()` should have rank 4. '
+                             'Got array with shape: ' + str(x.shape))
+        if x.shape[self.channel_axis] not in {1, 3, 4}:
+            warnings.warn(
+                'Expected input to be images (as Numpy array) '
+                'following the data format convention "' +
+                self.data_format + '" (channels on axis ' +
+                str(self.channel_axis) + '), i.e. expected '
+                'either 1, 3 or 4 channels on axis ' +
+                str(self.channel_axis) + '. '
+                'However, it was passed an array with shape ' +
+                str(x.shape) + ' (' + str(x.shape[self.channel_axis]) +
+                ' channels).')
+
+        if seed is not None:
+            np.random.seed(seed)
+
+        x = np.copy(x)
+        if augment:
+            ax = np.zeros(
+                tuple([rounds * x.shape[0]] + list(x.shape)[1:]),
+                dtype=K.floatx())
+            for r in range(rounds):
+                for i in range(x.shape[0]):
+                    ax[i + r * x.shape[0]] = self.random_transform(x[i])
+            x = ax
+
+        if self.featurewise_center:
+            self.mean = np.mean(x, axis=(0, self.row_axis, self.col_axis))
+            broadcast_shape = [1, 1, 1]
+            broadcast_shape[self.channel_axis - 1] = x.shape[self.channel_axis]
+            self.mean = np.reshape(self.mean, broadcast_shape)
+            x -= self.mean
+
+        if self.featurewise_std_normalization:
+            self.std = np.std(x, axis=(0, self.row_axis, self.col_axis))
+            broadcast_shape = [1, 1, 1]
+            broadcast_shape[self.channel_axis - 1] = x.shape[self.channel_axis]
+            self.std = np.reshape(self.std, broadcast_shape)
+            x /= (self.std + K.epsilon())
+
+        if self.zca_whitening:
+            flat_x = np.reshape(
+                x, (x.shape[0], x.shape[1] * x.shape[2] * x.shape[3]))
+            sigma = np.dot(flat_x.T, flat_x) / flat_x.shape[0]
+            u, s, _ = linalg.svd(sigma)
+            s_inv = 1. / np.sqrt(s[np.newaxis] + self.zca_epsilon)
+            self.principal_components = (u * s_inv).dot(u.T)
