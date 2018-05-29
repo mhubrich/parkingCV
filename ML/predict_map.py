595c74413c45974dc3e7b4a090d4e21f80779d2c
Markus Hubrich <mhubrich.2011@gmail.com>
2018-05-28 18:16:44 -0700

    function  creates a probability map of predictions of a high resolution image using the sliding window approach

---
+++ ML/predict_map.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from utils.sliding_window import SlidingWindow
+
+
+def predict_map(map, predict, window_h, window_w, stride, batch_size, **kwargs):
+    """Creates a probability map of predictions of the high resolution input
+    image `map` using a sliding window approach.
+        Arguments:
+          map: Array. Input image with dimensions of (height, width, 6).
+          predict: Function. It takes a batch of images as first input argument
+                   followed by `**kwargs` arguments. It returns exactly one
+                   probability per batch sample. Hence, for an input of
+                   len(batch) = n, it returns a list of n floats.
+                   For example, this function could be choosen to be
+                   * a model making predictions of the given batch
+                   * the averaged predictions of augmentations of the batch
+                   * predictions of an ensemble of classifiers
+          window_h: Integer. Height of the sliding window.
+          window_w: Integer. Width of the sliding window.
+          stride: Integer. Step size of the sliding window.
+          kwargs: Arguments passed to function `predict`.
+        Returns:
+          A probability map of predictions of `map`.
+    """
+    iter = SlidingWindow(map, window_h, window_w, stride)
+    map_proba = np.zeros(iter.img.shape[:2], dtype=np.float32)
+    map_counts = np.zeros(iter.img.shape[:2], dtype=np.int32)
+    for i in range(int(np.ceil(iter.n / float(batch_size)))):
+        num_samples = min((i+1) * batch_size, iter.n) - i * batch_size
+        batch_windows = np.zeros((num_samples,) + iter.window_size, dtype=np.uint8)
+        batch_pos = np.zeros((num_samples, 2), dtype=np.int32)
+        for j in range(num_samples):
+            window, pos = iter.next()
+            batch_windows[j] = window
+            batch_pos[j] = pos
+        predicitons = predict(batch_windows, **kwargs)
+        for j in range(len(predicitons)):
+            y1, y2 = batch_pos[j, 0], batch_pos[j, 0] + window_h
+            x1, x2 = batch_pos[j, 1], batch_pos[j, 1] + window_w
+            map_proba[y1:y2, x1:x2] = predicitons[j]
+            map_counts[y1:y2, x1:x2] += 1
+    map_proba /= map_counts
+    return map_proba[:map.shape[0], :map.shape[1]]
--- utils/sliding_window.py
+++ utils/sliding_window.py
@@ -27,19 +27,21 @@ class SlidingWindow(object):
         self.window_h = window_h
         self.window_w = window_w
         self.stride = stride
+        self.window_size = (window_h, window_w, img.shape[2])
         steps_y = int(np.ceil((img.shape[0] - window_h) / float(stride) + 1))
         steps_x = int(np.ceil((img.shape[1] - window_w) / float(stride) + 1))
         pad_y = (steps_y - 1) * stride + window_h - img.shape[0]
         pad_x = (steps_x - 1) * stride + window_w - img.shape[1]
         self.img = np.pad(img, pad_width=((0, pad_y), (0, pad_x), (0, 0)), mode=mode)
         self.index_array = [(y, x) for y in range(steps_y) for x in range(steps_x)]
+        self.n = len(self.index_array)
         self.count = 0
 
     def __iter__(self):
         return self
 
     def next(self):
-        if self.count >= len(self.index_array):
+        if self.count >= self.n:
             raise StopIteration
         pos_y = self.index_array[self.count][0] * self.stride
         pos_x = self.index_array[self.count][1] * self.stride
